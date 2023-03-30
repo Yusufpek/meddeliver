eeb3bc545dbc2ebe9dd30fca81f3d6e6ad99da22
Yusufpek <yusuf55ipek55@gmail.com>
2023-03-30 15:32:22 +0300

    bottom nav bar added (main page updated)

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:meddeliver/utils/context_extension.dart';
+import 'package:spincircle_bottom_bar/modals.dart';
+import 'package:spincircle_bottom_bar/spincircle_bottom_bar.dart';
 
-class MainPaeg extends StatelessWidget {
-  const MainPaeg({super.key});
+import '../utils/constant_images.dart';
+import 'home_page.dart';
+
+// ignore: constant_identifier_names
+enum Pages { HOME, DETAIL, HEALTH, PROFILE }
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  Pages currentPage = Pages.HOME;
+
+  Widget currentPageWidget() {
+    switch (currentPage) {
+      case Pages.HOME:
+        return const HomePage();
+      default:
+        return const Text("no page");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: customAppBar(),
+        //Adding SpinCircleBottomBarHolder to body of Scaffold
+        body: SpinCircleBottomBarHolder(
+          bottomNavigationBar: SCBottomBarDetails(
+            circleColors: [Colors.white, Colors.green, Colors.green[900]!],
+            iconTheme: const IconThemeData(size: 32, color: Colors.black45),
+            activeIconTheme: IconThemeData(size: 36, color: Colors.green[900]),
+            elevation: 2.0,
+            items: [
+              pageButtonItem(icon: Icons.home, page: Pages.HOME),
+              pageButtonItem(
+                  icon: Icons.text_snippet_outlined, page: Pages.DETAIL),
+              pageButtonItem(icon: Icons.favorite_border, page: Pages.HEALTH),
+              pageButtonItem(icon: Icons.person, page: Pages.PROFILE),
+            ],
+            actionButtonDetails: SCActionButtonDetails(
+              color: Colors.green,
+              icon: const Icon(Icons.add),
+              elevation: 2,
+            ),
+            circleItems: [
+              circleItem(Icons.qr_code_2_outlined),
+              circleItem(Icons.message),
+            ],
+            bnbHeight: context.screenHeight * 0.08,
+          ),
+          child: currentPageWidget(),
+        ),
+      ),
+    );
   }
+
+  Icon darkGreenIcon(IconData iconData) =>
+      Icon(iconData, color: Colors.green[800]);
+
+  SCItem circleItem(IconData iconData) =>
+      SCItem(icon: darkGreenIcon(iconData), onPressed: () {});
+
+  SCBottomBarItem pageButtonItem({
+    IconData icon = Icons.numbers,
+    Pages page = Pages.HOME,
+  }) =>
+      SCBottomBarItem(
+        icon: icon,
+        onPressed: () {
+          setState(() {
+            currentPage = page;
+          });
+        },
+      );
+
+  Widget circleAvatarWidget() => CircleAvatar(
+        child: Image.network(ConstantImages.getInstance().profilePictureHint),
+      );
+
+  AppBar customAppBar() => AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xff00FB83),
+        leading: const Icon(Icons.calendar_today_outlined),
+        actions: [
+          circleAvatarWidget(),
+        ],
+      );
 }
